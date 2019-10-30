ebd354cc34450a6eeef6d9347958da03100b1451
Sebastian Wick <sebastian@sebastianwick.net>
2019-10-30 19:18:39 +0100

    build system cleanup part two
    * compile shaders as part of the cmake build
    * get rid of the bin/shader directory (containing prebuild shaders)
    * move bin/data/ to data/
    * move data/run.bat to scripts/Gothic2Notr.bat to mirror linux
    * finally get rid of bin/
    * copy scripts/Gothic2Notr.bat to binary directory to mirror linux
    * copy data/ to binary directory
    * adjusted appveyor.yml to generate the artifact from the binary
      directory
    * for linux libraries will end up in lib/, the binary in bin/ and
      Gothic2Notr.sh in the root to start the game
    * on Windows all dll and exe files go into the binary directory root
    
    The result is that the binary directory now contains everything required
    to start OpenGothic from there while the source directory stays
    completely clean.

--- scripts/Gothic2Notr.sh
+++ scripts/Gothic2Notr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export LD_LIBRARY_PATH="$DIR:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$DIR/lib:$LD_LIBRARY_PATH"
 if [[ $DEBUGGER != "" ]]; then
-  exec $DEBUGGER --args "$DIR/Gothic2Notr" "$@"
+  exec $DEBUGGER --args "$DIR/bin/Gothic2Notr" "$@"
 else
-  exec "$DIR/Gothic2Notr" "$@"
+  exec "$DIR/bin/Gothic2Notr" "$@"
 fi
