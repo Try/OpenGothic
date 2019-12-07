20148fccb3d92e6f8569ac9271a9be896f7a48f7
Try <try9998@gmail.com>
2019-12-07 23:58:36 +0100

    fixup

--- Game/resources.cpp
+++ Game/resources.cpp
@@ -100,9 +100,9 @@ Resources::Resources(Gothic &gothic, Tempest::Device &device)
     gothicAssets.loadVDF(i);
   gothicAssets.finalizeLoad();
 
-  auto v = getFileData("font_old_20_white.tga");
-  Tempest::WFile f("../../internal/font_old_20_white.tga");
-  f.write(v.data(),v.size());
+  //auto v = getFileData("font_old_20_white.tga");
+  //Tempest::WFile f("../../internal/font_old_20_white.tga");
+  //f.write(v.data(),v.size());
   }
 
 Resources::~Resources() {
