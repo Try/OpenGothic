f02e687bbf4f33ad10f5214a3a7bb289f8c78a6f
Sebastian Lindgren <sebbestune@icloud.com>
2020-07-03 08:02:17 +0300

    Update Gothic2Notr.sh
    A simpler version that do not require an unchanged project name. This works when testing to start the game directly from build folder in Linux.

--- scripts/Gothic2Notr.sh
+++ scripts/Gothic2Notr.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
+function cdroot()
+{
+  cd ..; 
+  echo ${PWD}
+}
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export LD_LIBRARY_PATH="$DIR/lib:$LD_LIBRARY_PATH"
+
+
+export LD_LIBRARY_PATH=(cdroot)+"/lib:$LD_LIBRARY_PATH"
 if [[ $DEBUGGER != "" ]]; then
-  exec $DEBUGGER --args "$DIR/bin/Gothic2Notr" "$@"
+  exec $DEBUGGER --args "$DIR/Gothic2Notr" "$@"
 else
-  exec "$DIR/bin/Gothic2Notr" "$@"
+  exec "$DIR/Gothic2Notr" "$@"
 fi
