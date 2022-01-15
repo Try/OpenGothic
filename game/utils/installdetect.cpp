a0fa71724fabfd30ca0255d1a60598e58c378848
Try <try9998@gmail.com>
2022-01-15 18:25:56 +0300

    improve install detect on windows

--- game/utils/installdetect.cpp
+++ game/utils/installdetect.cpp
@@ -13,25 +13,35 @@
 
 InstallDetect::InstallDetect() {
 #ifdef __WINDOWS__
-  pfiles = programFiles();
+  pfiles    = programFiles(false);
+  pfilesX86 = programFiles(true);
 #endif
   }
 
 std::u16string InstallDetect::detectG2() {
-  auto akela = pfiles+u"/Akella Games/Gothic II/";
-  if(FileUtil::exists(akela))
-    return akela;
+  auto ret = detectG2(pfiles);
+  if(ret.empty())
+    ret = detectG2(pfilesX86);
+  return ret;
+  }
+
+std::u16string InstallDetect::detectG2(std::u16string pfiles) {
+  if(pfiles.empty())
+    return u"";
   auto steam = pfiles+u"/Steam/steamapps/common/Gothic II/";
   if(FileUtil::exists(steam))
     return steam;
+  auto akela = pfiles+u"/Akella Games/Gothic II/";
+  if(FileUtil::exists(akela))
+    return akela;
   return u"";
   }
 
 #ifdef __WINDOWS__
-std::u16string InstallDetect::programFiles() {
+std::u16string InstallDetect::programFiles(bool x86) {
   WCHAR path[MAX_PATH]={};
-  if(FAILED(SHGetFolderPathW(NULL, CSIDL_PROGRAM_FILES, NULL, 0, path)))
-     return u"";
+  if(FAILED(SHGetFolderPathW(NULL, (x86 ? CSIDL_PROGRAM_FILESX86 : CSIDL_PROGRAM_FILES), NULL, 0, path)))
+    return u"";
   std::u16string ret;
   size_t len=0;
   for(;path[len];++len);
--- game/utils/installdetect.h
+++ game/utils/installdetect.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Tempest/Platform>
+#include <filesystem>
 #include <string>
 
 class InstallDetect final {
@@ -10,8 +11,10 @@ class InstallDetect final {
     std::u16string detectG2();
 
   private:
+    std::u16string detectG2(std::u16string pfiles);
+
 #ifdef __WINDOWS__
-    std::u16string programFiles();
+    std::u16string programFiles(bool x86);
 #endif
-    std::u16string pfiles;
+    std::u16string pfiles, pfilesX86;
   };
