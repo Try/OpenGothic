807badaa31a1073c0a072e056ffd4c6262291c82
Luis Michaelis <private@lmichaelis.de>
2022-07-01 18:30:43 +0200

    Migrate `CameraDefinitions` from `Daedalus::GEngineClasses` to `phoenix::daedalus`

--- game/camera.h
+++ game/camera.h
@@ -4,7 +4,8 @@
 #include <Tempest/Matrix4x4>
 #include <Tempest/Point>
 #include <array>
-#include <daedalus/DaedalusStdlib.h>
+
+#include <phoenix/ext/daedalus_classes.hh>
 
 class World;
 class Npc;
@@ -131,5 +132,5 @@ class Camera final {
     void                  followAng   (Tempest::Vec3& spin, Tempest::Vec3 dest, float dtF);
     static void           followAng   (float& ang, float dest, float speed, float dtF);
 
-    const Daedalus::GEngineClasses::CCamSys& cameraDef() const;
+    const phoenix::daedalus::c_camera& cameraDef() const;
   };
--- game/game/definitions/cameradefinitions.h
+++ game/game/definitions/cameradefinitions.h
@@ -1,34 +1,31 @@
 #pragma once
 
-#include <daedalus/DaedalusStdlib.h>
+#include <phoenix/ext/daedalus_classes.hh>
 
 class CameraDefinitions final {
   public:
     CameraDefinitions();
 
-    const Daedalus::GEngineClasses::CCamSys& dialogCam()    const { return camModDialog; }
-    const Daedalus::GEngineClasses::CCamSys& inventoryCam() const { return camModInventory; }
-    const Daedalus::GEngineClasses::CCamSys& stdCam()       const { return camModNormal; }
-    const Daedalus::GEngineClasses::CCamSys& fpCam()        const { return camModFp; }
-    const Daedalus::GEngineClasses::CCamSys& backCam()      const { return camModBack; }
-    const Daedalus::GEngineClasses::CCamSys& meleeCam()     const { return camModMelee;  }
-    const Daedalus::GEngineClasses::CCamSys& rangeCam()     const { return camModRange;  }
-    const Daedalus::GEngineClasses::CCamSys& mageCam()      const { return camModMage;   }
-    const Daedalus::GEngineClasses::CCamSys& deathCam()     const { return camModDeath;  }
-    const Daedalus::GEngineClasses::CCamSys& swimCam()      const { return camModSwim;   }
-    const Daedalus::GEngineClasses::CCamSys& diveCam()      const { return camModDive;   }
-    const Daedalus::GEngineClasses::CCamSys& mobsiCam(std::string_view tag, std::string_view pos) const;
+    const phoenix::daedalus::c_camera& dialogCam()    const { return *camModDialog; }
+    const phoenix::daedalus::c_camera& inventoryCam() const { return *camModInventory; }
+    const phoenix::daedalus::c_camera& stdCam()       const { return *camModNormal; }
+    const phoenix::daedalus::c_camera& fpCam()        const { return *camModFp; }
+    const phoenix::daedalus::c_camera& backCam()      const { return *camModBack; }
+    const phoenix::daedalus::c_camera& meleeCam()     const { return *camModMelee;  }
+    const phoenix::daedalus::c_camera& rangeCam()     const { return *camModRange;  }
+    const phoenix::daedalus::c_camera& mageCam()      const { return *camModMage;   }
+    const phoenix::daedalus::c_camera& deathCam()     const { return *camModDeath;  }
+    const phoenix::daedalus::c_camera& swimCam()      const { return *camModSwim;   }
+    const phoenix::daedalus::c_camera& diveCam()      const { return *camModDive;   }
+    const phoenix::daedalus::c_camera& mobsiCam(std::string_view tag, std::string_view pos) const;
 
   private:
-    Daedalus::GEngineClasses::CCamSys loadCam(std::string_view name);
+    phoenix::daedalus::c_camera* getCam(std::string_view name);
 
-    struct Camera:Daedalus::GEngineClasses::CCamSys{
-      std::string name;
-      };
-    std::vector<Camera> cameras;
-    const Camera* find(std::string_view name) const;
+    std::vector<std::pair<std::string, std::shared_ptr<phoenix::daedalus::c_camera>>> cameras;
+    const phoenix::daedalus::c_camera* find(std::string_view name) const;
 
-    Daedalus::GEngineClasses::CCamSys camModNormal, camModFp, camModBack;
-    Daedalus::GEngineClasses::CCamSys camModDialog, camModInventory, camModDeath, camModSwim, camModDive;
-    Daedalus::GEngineClasses::CCamSys camModMelee, camModRange, camModMage;
+    phoenix::daedalus::c_camera* camModNormal, *camModFp, *camModBack;
+    phoenix::daedalus::c_camera* camModDialog, *camModInventory, *camModDeath, *camModSwim, *camModDive;
+    phoenix::daedalus::c_camera* camModMelee, *camModRange, *camModMage;
   };
