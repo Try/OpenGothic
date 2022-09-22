637a6ed81d6f3a5106a1baed6e4f7ede6cd3b325
thokkat <100468436+thokkat@users.noreply.github.com>
2022-09-22 18:40:59 +0000

    open inventory while walking (#326)

--- game/ui/inventorymenu.cpp
+++ game/ui/inventorymenu.cpp
@@ -104,7 +104,7 @@ void InventoryMenu::close() {
   }
 
 void InventoryMenu::open(Npc &pl) {
-  if(pl.isDown() || pl.isMonster() || !pl.isStanding())
+  if(pl.isDown() || pl.isMonster() || pl.isInAir() || pl.isSlide() || (pl.interactive()!=nullptr))
     return;
   if(pl.weaponState()!=WeaponState::NoWeapon) {
     pl.stopAnim("");
