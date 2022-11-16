507bcd6ec1799ab4fb3c2cb14fbbb0d4b58c9a8a
Try <try9998@gmail.com>
2022-11-16 19:47:08 +0100

    DYLD_LIBRARY_PATH for OSX
    #358

--- scripts/Gothic2Notr.sh
+++ scripts/Gothic2Notr.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export LD_LIBRARY_PATH="$DIR:$LD_LIBRARY_PATH"
+export DYLD_LIBRARY_PATH="$DIR:$DYLD_LIBRARY_PATH"
 if [[ $DEBUGGER != "" ]]; then
   exec $DEBUGGER --args "$DIR/Gothic2Notr" "$@"
 else
