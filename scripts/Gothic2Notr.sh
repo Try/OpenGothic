9914e522dbbb12077328516550644813c7cf631a
Raphael Ahrens <tant.sinnister@googlemail.com>
2023-02-21 22:21:40 +0100

    Porting To FreeBSD: Bash is not always in /bin (#415)
    FreeBSD and other BSDs store ported software under /usr/local
    This includes bash.
    
    This change uses /usr/bin/env to find teh location of bash
    
    Co-authored-by: Raphael Ahrens <tant@tantalos.fritz.box>

--- scripts/Gothic2Notr.sh
+++ scripts/Gothic2Notr.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export LD_LIBRARY_PATH="$DIR:$LD_LIBRARY_PATH"
 export DYLD_LIBRARY_PATH="$DIR:$DYLD_LIBRARY_PATH"
