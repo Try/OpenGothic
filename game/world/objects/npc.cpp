c5abc3e093f879c167e1725a89d4523659672002
Try <try9998@gmail.com>
2023-03-14 21:08:38 +0300

    Update npc.cpp

--- game/world/objects/npc.cpp
+++ game/world/objects/npc.cpp
@@ -2918,9 +2918,9 @@ void Npc::moveItem(size_t id, Interactive &to, size_t count) {
 void Npc::sellItem(size_t id, Npc &to, size_t count) {
   if(id==owner.script().goldId()->index())
     return;
-  int32_t price = invent.sellPriceOf(id)*(int32_t)count;
+  int32_t price = invent.sellPriceOf(id);
   Inventory::trasfer(to.invent,invent,this,id,count,owner);
-  invent.addItem(owner.script().goldId()->index(),size_t(price),owner);
+  invent.addItem(owner.script().goldId()->index(),size_t(price)*count,owner);
   }
 
 void Npc::buyItem(size_t id, Npc &from, size_t count) {
