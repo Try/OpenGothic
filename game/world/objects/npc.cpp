72c2e3a9326d58b4d6a944b920c96a16e42a0ce4
thokkat <thokatt@gmail.com>
2023-07-03 02:09:52 +0200

    remove some weaponstate checks
    in npc attacking related functions
    
    prevents softlock if npc is in water

--- game/world/objects/npc.cpp
+++ game/world/objects/npc.cpp
@@ -1414,8 +1414,7 @@ bool Npc::implAttack(uint64_t dt) {
     return false;
     }
 
-  auto ws = weaponState();
-  if(ws==WeaponState::NoWeapon)
+  if(!fghAlgo.hasInstructions())
     return false;
 
   if(bodyStateMasked()==BS_STUMBLE) {
@@ -1429,9 +1428,7 @@ bool Npc::implAttack(uint64_t dt) {
     return true;
     }
 
-  if(!fghAlgo.hasInstructions()) {
-    return false;
-    }
+  auto ws = weaponState();
 
   FightAlgo::Action act = fghAlgo.nextFromQueue(*this,*currentTarget,owner.script());
 
@@ -2357,7 +2354,7 @@ void Npc::nextAiAction(AiQueue& queue, uint64_t dt) {
       break;
       }
     case AI_Attack:
-      if(currentTarget!=nullptr && weaponState()!=WeaponState::NoWeapon){
+      if(currentTarget!=nullptr) {
         if(!fghAlgo.fetchInstructions(*this,*currentTarget,owner.script()))
           queue.pushFront(std::move(act));
         }
