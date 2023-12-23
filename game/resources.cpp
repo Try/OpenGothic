50886246cd5bf42d660a929507085744b301c111
Try <try9998@gmail.com>
2023-12-23 01:43:02 +0100

    fixup

--- game/resources.cpp
+++ game/resources.cpp
@@ -127,7 +127,7 @@ void Resources::loadVdfs(const std::vector<std::u16string>& modvdfs, bool modFil
         Log::e("skip compressed archive: \"", TextCodec::toUtf8(i.name), "\"");
         continue;
         }
-#ifndef __IOS__
+#ifdef __IOS__
       // causes OOM on iPhone7
       if(i.name.find(u"Speech")!=std::string::npos)
         continue;
