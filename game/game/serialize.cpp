678558289731cf6b843e3a13a1fb499e42d70ea0
Try <try9998@gmail.com>
2024-05-18 14:06:23 +0200

    faster save games
    #616

--- game/game/serialize.cpp
+++ game/game/serialize.cpp
@@ -96,7 +96,7 @@ void Serialize::closeEntry() {
   if(entryBuf.empty())
     return;
 
-  mz_uint level  = entryBuf.size()>256 ? MZ_BEST_COMPRESSION : MZ_NO_COMPRESSION;
+  mz_uint level  = entryBuf.size()>256 ? MZ_BEST_SPEED : MZ_NO_COMPRESSION;
   mz_bool status = mz_zip_writer_add_mem(&impl, entryName.c_str(), entryBuf.data(), entryBuf.size(), level);
   entryBuf .clear();
   entryName.clear();
@@ -120,8 +120,8 @@ bool Serialize::implSetEntry(std::string_view fname) {
       if(entryName[i]=='/' && i+1<entryName.size()) {
         const char prev = entryName[i+1];
         entryName[i+1] = '\0';
-        mz_uint32 id = mz_uint32(-1);
-        if(!mz_zip_reader_locate_file_v2(&impl, entryName.c_str(), nullptr, 0, &id)) {
+        const auto it = outFileList.insert(entryName.c_str());
+        if(it.second) {
           mz_bool status = mz_zip_writer_add_mem(&impl, entryName.c_str(), NULL, 0, MZ_NO_COMPRESSION);
           if(!status)
             throw std::runtime_error("unable to allocate entry in game archive");
