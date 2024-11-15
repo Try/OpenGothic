583ec1fb3e7a83fe0b19d25f38858ad046dee838
Schlumpf7 <77499857+Schlumpf7@users.noreply.github.com>
2024-11-15 01:46:20 +0100

    cleanup

--- game/game/piewheelmenu.h
+++
@@ -1,9 +0,0 @@
-#ifndef PIEWHEELMENU_H
-#define PIEWHEELMENU_H
-
-#include <SDL2/SDL.h>
-
-// Function to render the radial menu
-void renderRadialMenu(SDL_Renderer* renderer, int selectedOption);
-
-#endif // PIEWHEELMENU_H
