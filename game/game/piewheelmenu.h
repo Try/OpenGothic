1f133cf84fcfcc4d2fe2589f7400b4797ad57b43
Schlumpf7 <77499857+Schlumpf7@users.noreply.github.com>
2024-11-15 01:03:26 +0100

    Update piewheelmenu.h

--- game/game/piewheelmenu.h
+++ game/game/piewheelmenu.h
@@ -3,17 +3,7 @@
 
 #include <SDL2/SDL.h>
 
-class PieWheelMenu {
-public:
-    PieWheelMenu(SDL_Renderer* renderer, int screenWidth, int screenHeight);
-    void draw(int centerX, int centerY, int radius, int selectedOption);
-    void handleControllerInput(SDL_GameController* controller, int& selectedOption);
-
-private:
-    SDL_Renderer* renderer;
-    int screenWidth;
-    int screenHeight;
-    const int numOptions = 4; // Number of options in the pie menu
-};
+// Function to render the radial menu
+void renderRadialMenu(SDL_Renderer* renderer, int selectedOption);
 
 #endif // PIEWHEELMENU_H
