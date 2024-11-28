75f771e5fe560398e0ffeeed73c1a123cfb1ad77
Schlumpf7 <77499857+Schlumpf7@users.noreply.github.com>
2024-11-28 20:07:54 +0100

    debug

--- game/game/playercontrol.cpp
+++ game/game/playercontrol.cpp
@@ -87,9 +87,8 @@ PlayerControl::PlayerControl(DialogMenu& dlg, InventoryMenu &inv)
     std::ifstream inFile("controller_config.json");
     if (inFile.is_open()) {
         std::string configStr((std::istreambuf_iterator<char>(inFile)), std::istreambuf_iterator<char>());
-        //h->load_binding_from_json(configStr);
-        //std::cout << "Loaded configuration from file." << std::endl;
-        std::cout << "Loading configuration manually is not yet implemented" << std::endl;
+        h->load_binding_from_json(configStr);
+        std::cout << "Loaded configuration from file." << std::endl;
     } else {
         std::cout << "No configuration file found. Starting configuration wizard..." << std::endl;
         if (auto dev = h->get_devices().front()) { // Assuming at least one device is connected
