aa7ffeb609f9af994a3e3ae06435303cbefa5666
thokkat <thokatt@gmail.com>
2025-06-21 23:36:41 +0200

    Mover: rework tick function and add some attributes
    Handle repeated trigger events:
    `MoverBehavior::SINGLE_KEYS` and `LOOP` only react to trigger event in
    idle state. `TOGGLE` and `OPEN_TIME` always react. `TRIGGER_CONTROL`
    shows confusing behavior while testing where sometimes they behaved like
    `TOGGLE` type and sometimes wouldn't react at all when sending multiple
    trigger events. For now treat like `SINGLE_KEY/LOOP`.
    
    Support for `MoverMessageType`:
    Defines to which keyframe a mover of type `MoverBehavior::SINGLE_KEYS`
    should move.
    
    Rework mover advance calculation:
    This allows changing the state e.g. from Open to Close while moving and
    is necessary for repeated trigger events.
    
    Implement some more attributes:
    
    autoRotate: If true mover "looks" in motion direction. Used exclusively
    for fish
    speedType: defines the global speed behavior. Types are handled like in
    CsCamera.
    lerpType: defines the curve the mover is following. Type `CURVE`  is
    similar to CsCamera Spline. `CURVE` type is only different to `LINEAR`
    for >3 keyframes which makes this exclusively to fish as well.
    sfxCloseStart: Sound to play once mover starts closing.

--- game/world/triggers/movetrigger.h
+++ game/world/triggers/movetrigger.h
@@ -20,28 +20,34 @@ class MoveTrigger : public AbstractTrigger {
     void tick(uint64_t dt) override;
 
   private:
-    void  moveEvent() override;
-    void  processTrigger(const TriggerEvent& evt, bool onTrigger);
-
-    void  setView    (MeshObjects::Mesh&& m);
-    void  emitSound  (std::string_view snd, bool freeSlot=true);
-    void  advanceAnim(uint32_t f0, uint32_t f1, float alpha);
-    float scaleRotSpeed (float speed) const;
-
-    void  invalidateView();
-
     enum State : int32_t {
       Idle         = 0,
       Loop         = 1,
       Open         = 2,
       OpenTimed    = 3,
       Close        = 4,
-      NextKey      = 5,
+      SingleKey    = 5,
       };
 
     struct KeyLen {
-      uint64_t ticks    = 0;
+      uint64_t ticks = 0;
       };
+
+    void     moveEvent() override;
+    void     preProcessTrigger(State prev = Idle);
+    void     postProcessTrigger();
+
+    void     setView(MeshObjects::Mesh&& m);
+    void     emitSound(std::string_view snd, bool freeSlot=true) const;
+    void     advanceAnim();
+    float    calcProgress(uint32_t f1, uint32_t f2) const;
+    auto     calcTransform(uint32_t f1, uint32_t f2, float a) const -> Tempest::Matrix4x4;
+    void     updateFrame();
+    uint32_t nextFrame(uint32_t i) const;
+    uint32_t prevFrame(uint32_t i) const;
+    float    scaleRotSpeed(float speed) const;
+
+    void     invalidateView();
 
     Tempest::Matrix4x4                   pos0;
     MeshObjects::Mesh                    view;
@@ -49,14 +55,19 @@ class MoveTrigger : public AbstractTrigger {
     std::vector<KeyLen>                  keyframes;
     std::vector<zenkit::AnimationSample> moverKeyFrames;
     zenkit::MoverBehavior                behavior;
+    zenkit::MoverSpeedType               speedType;
+    zenkit::MoverLerpType                lerpType;
+    bool                                 autoRotate;
     std::string                          sfxOpenStart;
     std::string                          sfxOpenEnd;
+    std::string                          sfxCloseStart;
     std::string                          sfxCloseEnd;
     std::string                          sfxMoving;
     std::string                          visualName;
-    float                                stayOpenTimeSec;
+    uint64_t                             stayOpenTime;
 
-    State                                state     = Idle;
-    uint64_t                             sAnim     = 0;
-    uint32_t                             frame     = 0;
+    State                                state       = Idle;
+    uint64_t                             frameTime   = 0;
+    uint32_t                             frame       = 0;
+    uint32_t                             targetFrame = uint32_t(-1);
   };
