Try/OpenGothic

commit b7f8edef7d2735b248df17a7244d4adb52df8f41

daily routine doesn't need a valid waypoint
example: Character/Story helper

game/world/objects/npc.cpp
@@ -2973,15 +2973,13 @@ const Npc::Routine& Npc::currentRoutine() const {
   auto time = owner.time();
   time = gtime(int32_t(time.hour()),int32_t(time.minute()));
   for(auto& i:routines) {
-    if(i.point==nullptr)
-      continue;
     if(i.end<i.start && (time<i.end || i.start<=time))
       return i;
     if(i.start<=time && time<i.end)
       return i;
     }
 
-  // take previous routine
+  // take a previous routine if none was found for current time
   const auto     day   = gtime(24,0).toInt();
   const Routine* prevR = nullptr;
   int64_t        delta = std::numeric_limits<int64_t>::max();
@@ -2990,7 +2988,7 @@ const Npc::Routine& Npc::currentRoutine() const {
     int64_t d = time.toInt() - i.end.toInt();
     if(d<0)
       d += day;
-    if(i.point && d<=delta && d>0) {
+    if(d<=delta && d>0) {
       prevR = &i;
       delta = d;
       }

game/game/gamescript.cpp
@@ -3226,7 +3226,7 @@ int GameScript::mob_hasitems(std::string_view tag, int item) {
 
 void GameScript::ta_min(std::shared_ptr<zenkit::INpc> npcRef, int start_h, int start_m, int stop_h, int stop_m, int action, std::string_view waypoint) {
   auto npc = findNpc(npcRef);
-  auto at  = world().findPoint(waypoint);
+  auto at  = world().findPoint(waypoint,false);
 
   if(npc!=nullptr)
     npc->addRoutine(gtime(start_h,start_m),gtime(stop_h,stop_m),uint32_t(action),at);

commit a7ebff34af6f86a76bb384f65775bb3488ffc188

improve npc daily routine selection
Some npcs don't have a complete 24h schedule or waypoints given are non-
existent. Vanilla engine hides those script bugs by taking data from the
previous routine.

Try to approximate this behavior by searching routines counter-clockwise
until valid data is found.

Fixes a regression introcued in b7f8ede that would make npcs disappear.

game/game/serialize.h
@@ -33,7 +33,7 @@ class SaveGameHeader;
 class Serialize {
   public:
     enum Version : uint16_t {
-      Current = 50
+      Current = 51
       };
     Serialize(Tempest::ODevice& fout);
     Serialize(Tempest::IDevice&  fin);

game/world/objects/npc.cpp
@@ -458,8 +458,12 @@ bool Npc::resetPositionToTA() {
   if(!isPlayer())
     setInteraction(nullptr,true);
 
-  if(routines.size()==0)
-    return true;
+  if(routines.size()==0) {
+    if(hnpc->wp.empty())
+      return false; else
+      return true;
+    }
+  hnpc->wp = "";
 
   attachToPoint(nullptr);
   clearAiQueue();
@@ -472,15 +476,18 @@ bool Npc::resetPositionToTA() {
   if(isPlayer())
     return true;
 
-  auto& rot = currentRoutine();
-  auto  at  = rot.point;
-  if(at==nullptr)
-    return false;
+  auto& rtn = currentRoutine();
+  auto  at  = rtn.point;
+  if(at==nullptr) {
+    at = findPreviousRoutine().point;
+    if(at==nullptr)
+      return false;
+    }
 
-  if(at->isLocked() && !isDead){
+  if(at->isLocked() && !isDead) {
     auto p = owner.findNextPoint(*at);
     if(p!=nullptr)
-      at=p;
+      at = p;
     }
   setPosition (at->position() );
   setDirection(at->direction());
@@ -3030,24 +3037,31 @@ const Npc::Routine& Npc::currentRoutine() const {
     if(i.start<=time && time<i.end)
       return i;
     }
+  return findPreviousRoutine(false);
+  }
 
-  // take a previous routine if none was found for current time
-  const auto     day   = gtime(24,0).toInt();
-  const Routine* prevR = nullptr;
-  int64_t        delta = std::numeric_limits<int64_t>::max();
+const Npc::Routine& Npc::findPreviousRoutine(bool validWp) const {
+  auto time = owner.time();
+  time = gtime(int32_t(time.hour()),int32_t(time.minute()));
   time = time.timeInDay();
+  const auto     day   = gtime(24,0).toInt();
+  const Routine* rtn   = nullptr;
+  int64_t        delta = std::numeric_limits<int64_t>::max();
+
   for(auto& i:routines) {
     int64_t d = time.toInt() - i.end.toInt();
+    if(validWp && i.point==nullptr)
+      continue;
     if(d<0)
       d += day;
-    if(d<=delta && d>0) {
-      prevR = &i;
+    if(d<=delta) {
+      rtn   = &i;
       delta = d;
       }
     }
 
-  if(prevR!=nullptr)
-    return *prevR;
+  if(rtn!=nullptr)
+    return *rtn;
 
   static Routine r;
   return r;

game/world/objects/npc.h
@@ -467,6 +467,7 @@ class Npc final {
     bool      performOutput(const AiQueue::AiAction &ai);
 
     auto      currentRoutine() const -> const Routine&;
+    auto      findPreviousRoutine(bool validWp = true) const -> const Routine&;
     gtime     endTime(const Routine& r) const;
 
     bool      implPointAt(const Tempest::Vec3& to);

game/world/worldobjects.cpp
@@ -74,12 +74,24 @@ void WorldObjects::load(Serialize &fin) {
   itemArr.clear();
   items.clear();
 
-  uint32_t sz = fin.directorySize("worlds/",fin.worldName(),"/npc/");
+  uint32_t sz = 0;
+  if(fin.version()>50)
+    fin.read(sz); else
+    sz = fin.directorySize("worlds/",fin.worldName(),"/npc/");
   npcArr.resize(sz);
-  for(size_t i=0; i<sz; ++i)
+  for(size_t i=0; i<npcArr.size(); ++i)
     npcArr[i] = std::make_unique<Npc>(owner,size_t(-1),"");
   for(size_t i=0; i<npcArr.size(); ++i) {
     npcArr[i]->load(fin,i);
+    }
+
+  if(fin.version()>50) {
+    uint32_t szInv = fin.directorySize("worlds/",fin.worldName(),"/npc/") - sz;
+    npcInvalid.resize(szInv);
+    for(size_t i=0; i<npcInvalid.size(); ++i)
+      npcInvalid[i] = std::make_unique<Npc>(owner,size_t(-1),"");
+    for(size_t i=0; i<npcInvalid.size(); ++i)
+      npcInvalid[i]->load(fin,i+sz);
     }
 
   fin.setEntry("worlds/",fin.worldName(),"/items");
@@ -115,13 +127,14 @@ void WorldObjects::save(Serialize &fout) {
   fout.setEntry("worlds/",fout.worldName(),"/version");
   fout.write(Serialize::Version::Current);
 
-  for(size_t i=0; i<npcArr.size(); ++i) {
+  fout.write(uint32_t(npcArr.size()));
+  for(size_t i=0; i<npcArr.size(); ++i)
     npcArr[i]->save(fout,i);
-    }
+  for(size_t i=0; i<npcInvalid.size(); ++i)
+    npcInvalid[i]->save(fout,i+npcArr.size());
 
   fout.setEntry("worlds/",fout.worldName(),"/items");
-  uint32_t sz = uint32_t(itemArr.size());
-  fout.write(sz);
+  fout.write(uint32_t(itemArr.size()));
   for(auto& i:itemArr)
     i->save(fout);
 
@@ -275,10 +288,7 @@ uint32_t WorldObjects::mobsiId(const void* ptr) const {
 
 Npc* WorldObjects::addNpc(size_t npcInstance, std::string_view at) {
   auto pos = owner.findPoint(at);
-  if(pos==nullptr)
-    Log::e("addNpc: invalid waypoint");
-
-  Npc* npc = new Npc(owner,npcInstance,at);
+  Npc* npc = new Npc(owner,npcInstance,pos!=nullptr ? at : "");
   if(pos!=nullptr && pos->isLocked()){
     auto p = owner.findNextPoint(*pos);
     if(p)
@@ -952,7 +962,9 @@ void WorldObjects::resetPositionToTA() {
     r.curState = 0;
 
   for(auto& i:npcInvalid)
-    npcArr.push_back(std::move(i));
+    if(i->handlePtr().use_count()>1)
+      npcArr.push_back(std::move(i)); else
+      npcRemoved.push_back(std::move(i));
   npcInvalid.clear();
 
   for(size_t i=0;i<npcArr.size();) {