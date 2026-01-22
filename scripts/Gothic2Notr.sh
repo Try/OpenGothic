5aaefe657cb91bcd0b41e9dae623333864911f54
Michael Langfermann <versable@gmail.com>
2026-01-22 16:43:39 +0100

    build: statically link Tempest

--- scripts/Gothic2Notr.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export LD_LIBRARY_PATH="$DIR:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="$DIR:$DYLD_LIBRARY_PATH"
-if [[ $DEBUGGER != "" ]]; then
-  exec $DEBUGGER --args "$DIR/Gothic2Notr" "$@"
-else
-  exec "$DIR/Gothic2Notr" "$@"
-fi
