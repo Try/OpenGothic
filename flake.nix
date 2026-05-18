f736706e72868c648575377807f06cf3bd9269a7
pancakedevourer <szajagaja@gmail.com>
2026-05-18 02:48:09 +0200

    Added Nix dev flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {self, nixpkgs, flake-utils, ...}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            git
+            cmake
+            glslang
+            addDriverRunpath
+          ];
+          buildInputs = with pkgs; [
+            alsa-lib
+            libX11
+            libXcursor
+            libglvnd
+            vulkan-headers
+            vulkan-loader
+            vulkan-validation-layers
+          ];
+        };
+      });
+}
