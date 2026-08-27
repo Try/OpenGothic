a4d16ef4af9d6ef8ac2ac2003ff2a9f143b44a8c
Voron25 <59234589+Voron25@users.noreply.github.com>
2026-08-27 20:27:27 +0200

    WIP: add mesh-based viewport selection

--- spacer/ui/objects/worldedit.h
+++ spacer/ui/objects/worldedit.h
@@ -2,6 +2,10 @@
 
 #include <string_view>
 #include <memory>
+#include <unordered_map>
+
+#include <Tempest/Matrix4x4>
+#include <Tempest/Vec>
 
 #include <zenkit/vobs/VirtualObject.hh>
 
@@ -9,6 +13,7 @@
 
 class DynamicWorld;
 class WorldView;
+class StaticMesh;
 
 class WorldEdit {
   public:
@@ -17,19 +22,32 @@ class WorldEdit {
 
     WorldView& view() { return *wview; }
 
+    bool                    select(const Tempest::Vec3& rayOrigin, const Tempest::Vec3& rayDirection, float maxDistance);
+    bool                    hasSelection() const;
+    Tempest::Matrix4x4      selectedTransform() const;
+    void                    setSelectedTransform(const Tempest::Matrix4x4& transform);
+    std::string_view        selectedName() const;
+
   private:
     struct Vob {
       std::vector<Vob>                       child;
       std::shared_ptr<zenkit::VirtualObject> orig;
 
       MeshObjects::Mesh mesh;
+      Tempest::Matrix4x4 transform;
       };
+
+    struct PickMesh;
 
     void load(Vob& out, std::vector<std::shared_ptr<zenkit::VirtualObject>>& child);
     void initView(Vob& out);
+    const PickMesh& pickMesh(const StaticMesh& mesh);
+    bool rayMesh(Vob& vob, const Tempest::Vec3& rayOrigin, const Tempest::Vec3& rayDirection, float& distance);
 
     Vob                           root;
+    std::vector<Vob*>             selectable;
+    Vob*                          selected = nullptr;
+    std::unordered_map<const StaticMesh*,std::shared_ptr<PickMesh>> pickMeshes;
     std::unique_ptr<DynamicWorld> physics;
     std::unique_ptr<WorldView>    wview;
   };
-
