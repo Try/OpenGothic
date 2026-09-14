794d797183d80cfcf295943b1c6013a30da918ed
Constantine Romakhov <34831419+Solessfir@users.noreply.github.com>
2026-09-14 22:28:50 +0200

    Avoid redundant ZIP metadata decoding when loading saves (#988)
    * Read save archives without redundant ZIP metadata decoding
    
    * nitpicks
    
    ---------
    
    Co-authored-by: Try <try9998@gmail.com>

--- common/game/serialize.h
+++ common/game/serialize.h
@@ -45,7 +45,7 @@ class Serialize {
       };
     Serialize(Tempest::ODevice& fout);
     Serialize(Tempest::IDevice&  fin);
-    Serialize(Serialize&&)=default;
+    Serialize(Serialize&&)=delete;
     ~Serialize();
 
     uint16_t version()              const { return wldVer; }
