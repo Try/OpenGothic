Try/OpenGothic

commit 1ac890a11cc771d8fb30f9d5afc377ad54434834

Fix cursor visibility on startup and focus (#980)
* Fix cursor visibility on startup and focus

* Fix cursor visibility in fullscreen mode

* cancel submodule change (no dependency there)

---------

Co-authored-by: Try <try9998@gmail.com>

common/mainwindow.cpp
@@ -44,8 +44,10 @@ MainWindow::MainWindow(Device& device)
     setWindowTitle("Gothic II"); else
     setWindowTitle("Gothic");
 
-  if(!CommandLine::inst().isWindowMode())
+  if(!CommandLine::inst().isWindowMode()) {
     setFullscreen(true);
+    setCursorShape(CursorShape::Hidden);
+    }
 
   //renderer.resetSwapchain();
   setupUi();
@@ -295,10 +297,10 @@ void MainWindow::resizeEvent(SizeEvent&) {
   if(auto camera = Gothic::inst().camera())
     camera->setViewport(swapchain.w(),swapchain.h());
 
-  const bool fs = SystemApi::isFullscreen(hwnd());
-  auto rect = SystemApi::windowClientRect(hwnd());
-  setCursorPosition(rect.w/2,rect.h/2);
-  setCursorShape(fs ? CursorShape::Hidden : CursorShape::Arrow);
+  if(SystemApi::isFullscreen(hwnd())) {
+    setCursorPosition(Point(w()/2,h()/2));
+    }
+
   dMouse = Point();
   }
 
@@ -560,8 +562,9 @@ void MainWindow::focusEvent(FocusEvent &event) {
   if(!event.in)
     return;
   dMouse = Point();
-  auto center = Point(w()/2,h()/2);
-  setCursorPosition(center);
+
+  if(SystemApi::isFullscreen(hwnd()))
+    setCursorPosition(Point(w()/2,h()/2));
   }
 
 void MainWindow::paintFocus(Painter& p, const Focus& focus, const Matrix4x4& vp) {

commit fbb4794e97e8434784c4d5d1392751e3d6b930df

fix regression with cursor introduced in (#980)

common/mainwindow.cpp
@@ -46,7 +46,6 @@ MainWindow::MainWindow(Device& device)
 
   if(!CommandLine::inst().isWindowMode()) {
     setFullscreen(true);
-    setCursorShape(CursorShape::Hidden);
     }
 
   //renderer.resetSwapchain();
@@ -1213,6 +1212,7 @@ void MainWindow::clearInput() {
 
 void MainWindow::setFullscreen(bool fs) {
   SystemApi::setAsFullscreen(hwnd(),fs);
+  setCursorShape(fs ? CursorShape::Hidden : CursorShape::Arrow);
   }
 
 void MainWindow::render(){